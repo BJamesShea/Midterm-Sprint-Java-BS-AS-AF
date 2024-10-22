9ab0ed53399b00e63a3ed1dfacccc4d1ec3a0e19
Angela Flynn <Angela.Flynn@keyin.com>
2024-10-21 21:45:21 -0230

    Added content to Book class as a subclass of LibraryItem class

--- src/LibraryManagement/items/Book.java
+++ src/LibraryManagement/items/Book.java
@@ -1,4 +1,30 @@
 package LibraryManagement.items;
 
+import LibraryManagement.authors.Author;
+
 public class Book extends LibraryItem{
+
+    // Private instance variables
+    private String format; // Format of the book (Printed, Electronic, or Audio)
+
+    // Constructor
+    public Book(String id, String title, Author author, String isbn, String publisher, int numberOfCopies, String format) {
+        super(id, title, author, isbn, publisher, numberOfCopies);
+        this.format = format;
+    }
+
+    // Getters and Setters
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    // toString method for description that adds on to LibraryItem's toString method
+    @Override
+    public String toString() {
+        return super.toString() + ", Format: " + format;
+    }
 }
